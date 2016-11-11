8413491bc6ae5a373947311f5a24f1cb9619cafb
Robin Stocker <robin@nibor.org>
2016-11-11 22:45:57 +1100

    Add assertions to benches

--- benches/url.rs
+++ benches/url.rs
@@ -10,10 +10,11 @@ use test::Bencher;
 fn no_links(b: &mut Bencher) {
     let link_finder = LinkFinder::new();
     b.iter(|| {
-        link_finder.find(
+        let links = link_finder.find(
             "This is a text with no links in it. But: it has a colon.\
              Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
-             Curabitur luctus tincidunt diam.").count()
+             Curabitur luctus tincidunt diam.");
+        assert_eq!(links.count(), 0);
     });
 }
 
@@ -21,10 +22,11 @@ fn no_links(b: &mut Bencher) {
 fn some_links(b: &mut Bencher) {
     let link_finder = LinkFinder::new();
     b.iter(|| {
-        link_finder.find(
+        let links = link_finder.find(
             "This is a text with links in it. Here's one: http://www.example.com/. \
              How about another one? (Maybe like this http://example.com/foo_(bar)) \
-             a+b://example.com/foo+bar+baz").count()
+             a+b://example.com/foo+bar+baz");
+        assert_eq!(links.count(), 3);
     });
 }
 
@@ -32,10 +34,11 @@ fn some_links(b: &mut Bencher) {
 fn heaps_of_links(b: &mut Bencher) {
     let link_finder = LinkFinder::new();
     b.iter(|| {
-        link_finder.find(
+        let links = link_finder.find(
             "http://www.example.com/a, http://www.example.com/b, http://www.example.com/c \
              http://www.example.com/a: http://www.example.com/b: http://www.example.com/c \
              http://www.example.com/a http://www.example.com/b http://www.example.com/c \
-             http://www.example.com/a< http://www.example.com/b< http://www.example.com/c<").count()
+             http://www.example.com/a< http://www.example.com/b< http://www.example.com/c<");
+        assert_eq!(links.count(), 12);
     });
 }
