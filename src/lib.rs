d344c8914f704934f46997b8f916918ee2adb078
Robin Stocker <robin@nibor.org>
2017-02-12 21:30:58 +1100

    Add introduction to documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
-//! TODO.
+//! Find links such as URLs and email addresses in plain text.
+//! It's smart about where a link ends, such as with surrounding punctuation.
+//!
+//! Your first reaction might be: "That's easy, I can use a regex for that".
+//! So you write one that kind of works with a few cases, such as:
+//!
+//! * `http://example.org/.` should not include the trailing dot
+//! * `http://example.org/,` should not include the trailing comma
+//! * `(http://example.org)` should not include the parens
+//!
+//! But then does it also work in these cases:
+//!
+//! * `https://en.wikipedia.org/wiki/Link_(The_Legend_of_Zelda)` should include the trailing paren
+//! * `http://example.org/ä` should also work for Unicode of course
+//! * `<http://example.org/>` should not include angle brackets
+//!
+//! This crate behaves as you'd expect in the above cases.
+//!
+//! ### Usage
 //!
 //! ```
 //! use linkify::LinkFinder;
