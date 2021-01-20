0abd0f89d08d1eb0cabeb0f6ec48ebb52afa922a
Robin Stocker <robin@nibor.org>
2021-01-21 00:37:16 +1100

    Add some layout and styling

--- demo/index.js
+++ demo/index.js
@@ -1,20 +1,39 @@
 const pkg = import("./pkg");
 
-let rust = undefined;
+let rustModule = undefined;
 
 pkg
-  .then((m) => (rust = m))
+  .then((m) => {
+    rustModule = m;
+    // In case of a page reload, the input can contain remembered text, so linkify that.
+    handleInput(input.value);
+  })
   .catch((e) => console.error("Failed to load WebAssembly module", e));
 
-export function linkifyText(input) {
-  return rust !== undefined ? rust.linkify_text(input) : undefined;
+function linkifyText(input) {
+  return rustModule !== undefined ? rustModule.linkify_text(input) : undefined;
 }
 
-function handleInput(e) {
-  const text = e.target.value;
+function handleInput(text) {
+  const start = performance.now();
   output.innerHTML = linkifyText(text);
+  const millis = performance.now() - start;
+  if (text.length === 0) {
+    timing.innerHTML = "";
+  } else {
+    const links = document.getElementsByTagName("a").length;
+    timing.innerHTML =
+      "✓ <strong>" +
+      millis +
+      " ms</strong> to linkify " +
+      links +
+      " links in text with " +
+      text.length +
+      " characters";
+  }
 }
 
 const input = document.getElementById("input");
 const output = document.getElementById("output");
-input.oninput = handleInput;
+const timing = document.getElementById("timing");
+input.oninput = (e) => handleInput(e.target.value);
