10fe1d4564dee7a6faaeac077b1791825b3c6037
Matthias Endler <matthias-endler@gmx.net>
2021-04-19 15:08:50 +0200

    Add more single-quote tests

--- tests/url.rs
+++ tests/url.rs
@@ -130,6 +130,8 @@ fn matching_punctuation_tricky() {
 
 #[test]
 fn single_quote() {
+    assert_linked("'https://example.org'", "'|https://example.org|'");
+    assert_linked("''https://example.org''", "''|https://example.org|''");
     assert_linked("'https://example.org''", "'|https://example.org|''");
     assert_linked(
         "http://example.org/\'_(foo)",
