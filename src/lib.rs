917fc0872f440ff9b25921aedd842bd42d1d8fbc
Robin Stocker <robin@nibor.org>
2021-11-26 12:01:17 +1100

    Document in lib.rs as well

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,21 @@
 //! assert_eq!(&LinkKind::Url, link.kind());
 //! ```
 //!
+//! Option to allow URLs without schemes:
+//!
+//! ```
+//! use linkify::LinkFinder;
+//!
+//! let input = "Look, no scheme: example.org/foo";
+//! let mut finder = LinkFinder::new();
+//!
+//! // true by default
+//! finder.url_must_have_scheme(false);
+//!
+//! let links: Vec<_> = finder.links(input).collect();
+//! assert_eq!(links[0].as_str(), "example.org/foo");
+//! ```
+//!
 //! Restrict the kinds of links:
 //!
 //! ```
