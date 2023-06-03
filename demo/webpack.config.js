f869748fc8d02aa1b465041016b5d2d8fbbb6642
Robin Stocker <robin@nibor.org>
2023-06-03 17:03:49 +1000

    Try to fix build issue

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -9,6 +9,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
+    // Fix CI build issue with Node 18. Can be removed when upgrading to Webpack 5, see https://stackoverflow.com/a/73465262
+    hashFunction: "xxhash64",
   },
   plugins: [
     new HtmlWebpackPlugin({
